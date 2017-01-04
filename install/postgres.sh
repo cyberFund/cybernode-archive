d6becf82b380cadadaba23ed30f83f2e43998fe2
21xhipster <d@cyber.fund>
2017-01-04 15:49:20 +0800

    Remove legacy stuff from Java

--- install/postgres.sh
+++
@@ -1,7 +0,0 @@
-docker rmi -f cf/postgres
-docker build -t cf/postgres ./postgres
-
-docker stop postgres
-docker rm postgres
-docker run --name postgres -d -p 5432:5432 cf/postgres
-
--- install/postgres_.sh
+++
@@ -1,4 +0,0 @@
-docker stop postgres
-docker rm postgres
-docker run --name postgres -e POSTGRES_PASSWORD=gfhjkm -e POSTGRES_DB=chain -d -p 5432:5432 postgres
-
