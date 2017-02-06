3d72b4ea9973218a127ae18c2afe8c71ebb0230c
Andrey Lobarev <nxtpool@gmail.com>
2017-02-06 12:14:28 +0700

    Chain mock docker file.

---
+++ install/chain_mock_build_n_run.sh
@@ -0,0 +1,6 @@
+docker rmi -f chain_mock
+docker build -t chain_mock ./chain_mock
+docker stop chain_mock
+docker rm -v chain_mock
+docker run -d -p 8092:8092 --name chain_mock chain_mock
+docker attach --sig-proxy=false bitcore
