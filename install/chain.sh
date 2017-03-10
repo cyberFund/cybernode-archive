67b7e709ad7537273af004c2b42e13061efbb3c5
Andrey Lobarev <nxtpool@gmail.com>
2017-03-10 18:16:22 +0700

    Chain with cli_wallet in a docker container

--- install/chain.sh
+++ install/chain.sh
@@ -3,5 +3,5 @@ docker stop chain
 docker rm -v chain
 docker rmi -f chain
 docker build -t chain .
-docker run -dt --net host -v ~/golosnode/witness_node_data_dir:/golos --name chain chain
+docker run -dt --net host -v ~/golosnode:/golos --name chain chain
 docker attach --sig-proxy=false chain
--- install/chain/start.sh
+++ install/chain/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /usr/local/bin
-screen -dmS golosd ./golosd -d /golos
+screen -dmS golosd ./golosd -d /golos/witness_node_data_dir
 rl=0
 while [ $rl = 0 ]
 do
@@ -8,5 +8,5 @@ do
     r=$(curl -d '{"id":"1","method":"get_dynamic_global_properties","params":[""]}' 127.0.0.1:8090 | grep result)
     rl=${#r}
 done
-screen -dmS cliwallet ./cli_wallet --server-rpc-endpoint=ws://127.0.0.1:8090 --rpc-http-endpoint=127.0.0.1:8091 --rpc-http-allowip 127.0.0.1 -d
+screen -dmS cliwallet ./cli_wallet -s ws://127.0.0.1:8090 -H 127.0.0.1:8091 --rpc-http-allowip 127.0.0.1 -w /golos/wallet.json -d
 screen -r golosd
