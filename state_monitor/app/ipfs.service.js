7574a7089a8c2b35301a8c57bebe3dcbafa9ef59
Andrey Lobarev <nxtpool@gmail.com>
2017-04-14 19:10:27 +0700

    forgotten ipfs service

---
+++ state_monitor/app/ipfs.service.js
@@ -0,0 +1,16 @@
+var config = require('../config.json');
+var rpc = require('json-rpc2');
+var store = rpc.Client.$create(config.ipfsStore.port, config.ipfsStore.host);
+
+const BTC = 'btc';
+
+function getHeight(callback) {
+    store.call('getHeight', [BTC], function (data) {
+        if (data instanceof Error) {
+            log.warn('IPFS Get Heigh response:' + tdata);
+            callback(undefined);
+            return;
+        }
+        callback(height);
+    });
+}
