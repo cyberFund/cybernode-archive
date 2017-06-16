4b5bded18151075d6dcbe48aea973f0217b2c053
Anatoli Babenia <anatoli@rainforce.org>
2017-06-16 22:25:01 +0300

    Building final crawler image

--- images/xchange-crawler/build-image.sh
+++ images/xchange-crawler/build-image.sh
@@ -4,9 +4,16 @@
 set -e
 
 # path to script directory
-DIR=$(dirname "$0")
+DIR=$(dirname $(readlink -f "$0"))
+
+echo [build] ---- building xchange-crawler image ----
+
 cd $DIR
 
 IMAGE=xchange-crawler
 docker build -t ${IMAGE}-build -f Dockerfile-build . | tee build.log
 docker run --rm -v "$PWD"/bin:/build "${IMAGE}-build" | tee build-run.log
+ls -la "$PWD"/bin
+echo ... creating $IMAGE
+docker build -t $IMAGE . | tee build-$IMAGE.log
+cd -
