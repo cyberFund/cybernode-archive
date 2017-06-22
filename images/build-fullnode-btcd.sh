cyberFund/cybernode-archive

commit 4fae97dc2ce1cdd8ee1b978b9acb89ffe4b1a876

Move build script for btcd to image/, update docs

images/build-fullnode-btcd.sh
@@ -8,8 +8,8 @@ DIR=$(dirname $(readlink -f "$0"))
 
 echo [build] ---- building Bitcoin fullnode based on btcd ----
 
-BUILDDIR="$DIR"/btcd
-BINDIR="$DIR"/btcd/bin
+BUILDDIR="$DIR"/fullnode/btcd
+BINDIR=$BUILDIR/bin
 
 echo "Build Dir:   $BUILDDIR"
 

.travis.yml
@@ -4,5 +4,5 @@ services:
   - docker
 
 script:
-  - ./images/fullnode/build.sh
+  - ./images/build-fullnode-btcd.sh
   - ./images/xchange-crawler/build-image.sh

commit e8564cc2ed7d7b8a795d995eb0cde8d275f58648

Fix BINDIR variable for btcd build broken in 4fae97dc
The build worked, because $BINDIR was set to `/bin` but
resulting image included all system binaries.

images/build-fullnode-btcd.sh
@@ -9,7 +9,7 @@ DIR=$(dirname $(readlink -f "$0"))
 echo [build] ---- building Bitcoin fullnode based on btcd ----
 
 BUILDDIR="$DIR"/fullnode/btcd
-BINDIR=$BUILDIR/bin
+BINDIR=$BUILDDIR/bin
 
 echo "Build Dir:   $BUILDDIR"
 