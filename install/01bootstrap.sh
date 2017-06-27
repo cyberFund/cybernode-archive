38560571281da8e2b6494f4425e892439021647c
Anatoli Babenia <anatoli@rainforce.org>
2017-06-27 23:23:01 +0300

    Add 'cyberbuilder` user (#60)

--- install/01bootstrap.sh
+++ install/01bootstrap.sh
@@ -3,6 +3,9 @@
 # Init base Ubuntu 16.04 with `cyber` user, Docker and automatic
 # security updates. See [/docs/01bootstrap.md] for details.
 
+# This script is not idempotent and may leave some duplicate
+# settings if run twice.
+
 
 echo [x] Enable automatic security updates
 apt-get -y update
@@ -18,10 +21,25 @@ echo >> .bashrc
 echo "source /usr/share/bash-completion/bash_completion" >> .bashrc
 
 
-echo [x] Creating Cyber
+echo [x] Creating Cyber and CyberBuilder
+# cyber runs containers and owns cyberdata
 if id "cyber" >/dev/null 2>&1; then
   echo 'cyber' user already exists
 else
   # https://en.wikipedia.org/wiki/Gecos_field
   adduser cyber --disabled-password --gecos ""
 fi
+
+if id "cyberbuilder" >/dev/null 2>&1; then
+# cyberbuilder (re)builds containers and pushes them to DockerHub
+  echo 'cyberbuilder' user already exists
+else
+  adduser cyberbuilder --disabled-password --gecos ""
+  adduser cyberbuilder docker
+fi
+
+
+# swappiness
+
+# disk mounting
+
