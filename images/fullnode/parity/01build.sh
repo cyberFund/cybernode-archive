80e820ff7e2d6b27552f5c7c46a88f6a5c62597f
Anatoli Babenia <anatoli@rainforce.org>
2017-07-11 16:08:09 +0300

    Add Parity build infrastructure
    Since two our other build images (golang and maven) are Debian jessie
    based, it makes sense to use it for Rust build image too. More info
    about base `buildpacks-deps` images:
    
    https://github.com/docker-library/docs/tree/master/buildpack-deps#what-is-buildpack-deps

---
+++ images/fullnode/parity/01build.sh
@@ -0,0 +1,17 @@
+
+echo --- checking build tools ---
+rustc --version
+cargo --version
+echo $PATH
+
+echo --- clone Parity sources ---
+git clone https://github.com/paritytech/parity
+cd parity
+
+echo --- build in release mode to ./target/release ---
+cargo build --release
+
+# [ ] static compile for scratch image
+# record version
+git rev-parse HEAD > ./target/release/VERSION
+ls -la ./target/release
