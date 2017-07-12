2ef30d8402553a25966700ef690fddcc3cf2875d
Anatoli Babenia <anatoli@rainforce.org>
2017-07-12 15:32:25 +0300

    Expose `btcd` JSON-RPC port 8334 locally

--- images/run-fullnode-btcd.sh
+++ images/run-fullnode-btcd.sh
@@ -2,10 +2,14 @@ IMAGE=fullnode-btcd
 NAME=btcd
 VERSION=`docker inspect --format='{{.Config.Labels.version}}' $IMAGE`
 
+# -p 8333:8333  - host:container - expose port 8333 as 8333 from host
+#   8333 (Bitcoin P2P) is accessible from outside
+#   8334 (JSON-RPC/WS) only locally
+PORTS="-p 8333:8333 -p localhost:8334:8334"
+
 echo ... starting $NAME $VERSION from $IMAGE
-docker run -d --restart always -u $(id -u cyber) -p 8333:8333 --name $NAME -v /home/cyber/cyberdata/$NAME:/cyberdata $IMAGE
+docker run -d --restart always -u $(id -u cyber) $PORTS --name $NAME -v /home/cyber/cyberdata/$NAME:/cyberdata $IMAGE
 #    -d                  - run as daemon 
 #    -u $(id -u cyber)   - run container under user `cyber`, param needs uid
-#    -p 8333:8333        - host:container - expose port 8333 as 8333 from host
 #    --name btcd         - just convenient name to find running container
 #    -v .a.:.b.          - mount .a. in host as .b. in container
