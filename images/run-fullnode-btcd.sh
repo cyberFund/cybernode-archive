f93fe63be22332b69fc1b432be6554f229e4b2b4
Anatoli Babenia <anatoli@rainforce.org>
2017-08-15 17:31:04 +0300

    Warn if CYBERDATA is not set

--- images/run-fullnode-btcd.sh
+++ images/run-fullnode-btcd.sh
@@ -2,6 +2,11 @@
 # fail on error
 set -e
 
+
+# name of running container
+NAME=btcd
+
+
 # empty if `cyber` does not exist, set to `cyber` uid otherwise
 CYBER=$(id -u cyber 2>/dev/null) || true   # "|| true" ignores error
 if [ -z "$CYBER"]; then
@@ -12,9 +17,20 @@ else
   RUNUSER=-u $CYBER
 fi
 
+# if /cyberdata does not exist
+if [ -z "$CYBERDATA"]; then
+  if [ -z "$CYBER"]; then
+    echo "Error: set CYBERDATA to store `btcd` blockchain (150Gb+)."
+    echo "       Make sure to use separate partition from your root"
+    echo "       to avoid filling system disk and locking your system."
+    exit 1
+  else
+    CYBERDATA=/home/cyber/cyberdata/$NAME
+  fi
+fi
+
 
 IMAGE=fullnode-btcd
-NAME=btcd
 VERSION=`docker inspect --format='{{.Config.Labels.version}}' $IMAGE`
 
 # -p 8333:8333  - host:container - expose port 8333 as 8333 from host
@@ -23,7 +39,8 @@ VERSION=`docker inspect --format='{{.Config.Labels.version}}' $IMAGE`
 PORTS="-p 8333:8333 -p 127.0.0.1:8334:8334"
 
 echo ... starting $NAME $VERSION from $IMAGE
-docker run -d --restart always $RUNUSER $PORTS --name $NAME -v /home/cyber/cyberdata/$NAME:/cyberdata $IMAGE $*
+docker run -d --restart always $RUNUSER $PORTS --name $NAME -v $CYBERDATA:/cyberdata $IMAGE $*
 #    -d                  - run as daemon 
 #    --name btcd         - just convenient name to find running container
 #    -v .a.:.b.          - mount .a. in host as .b. in container
+
