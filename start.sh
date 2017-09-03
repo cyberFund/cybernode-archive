4587a98547da9e2f8c2733a9ad1995461e4b2bdc
Anatoli Babenia <anatoli@rainforce.org>
2017-09-03 14:36:17 +0300

    Wait for replicationcontroller to start pod with bash

--- start.sh
+++ start.sh
@@ -6,15 +6,19 @@ echo ...Starting k8s cluster...
 # [ ] minikube - choose different virt driver on MacOS
 minikube start --vm-driver=kvm
 
-echo ...Creating deployment for markets...
+echo ...Run replicationcontroller for markets container...
 echo "(running cm-app container on cluster)"
-# [ ] kubectl doesn't wait for pod to appear, so getting pods fails
-#     https://github.com/kubernetes/kubernetes/issues/51850
-kubectl run kubermarkets --image=cybernode/cm-app --port=80 --pod-running-timeout=1m0s
-kubectl get pods
+kubectl run kubermarkets --image=cybernode/cm-app --port=80
 
-echo Sleeping for 5 seconds to wait for pod to appear..
-sleep 5
+# [ ] kubectl doesn't wait for pod to appear
+#     https://github.com/kubernetes/kubernetes/issues/51850
+#     (alter default --pod-running-timeout for non-attached run)
+#     using custom bash instead
+while [[ -z `kubectl get pods -o name` ]]; do
+  echo Waiting for replication controller to start Pod
+  sleep 0.7
+done
+
 POD_NAME=$(kubectl get pods -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
 echo Now open http://127.0.0.1:8001/api/v1/proxy/namespaces/default/pods/$POD_NAME/
 kubectl proxy
