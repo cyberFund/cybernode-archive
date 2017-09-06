38cd4261896b149eb3e24e228851818251e262b2
Anatoli Babenia <anatoli@rainforce.org>
2017-09-06 17:53:34 +0300

    search.yaml cluster config with elassandra
        ./start.sh search.yaml

--- start.sh
+++ start.sh
@@ -10,8 +10,15 @@ minikube start --vm-driver=kvm
 
 # ability to run as ./script.sh markets.sh
 if [ $# -ne 0 ]; then
-  echo Running script '$*'
-  bash "$*"
+  if [ ${1: -3} == ".sh" ]; then
+    echo Running bash script "$*"
+    bash "$*"
+  else
+    if [ ${1: -5} == ".yaml" ]; then
+      echo Creating resources from kubectl config "$*"
+      kubectl create -f $*
+    fi
+  fi
 fi
 
 # cleanup
