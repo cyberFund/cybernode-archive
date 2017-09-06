42997e9d0c74fbbad6147035ffcbae0391390467
Anatoli Babenia <anatoli@rainforce.org>
2017-09-06 12:52:27 +0300

    Expose markets for public access on random port

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 echo ...Starting k8s cluster...
 # [ ] detect if started
 # [ ] detect if failed to start
@@ -20,8 +22,16 @@ while [[ -z `kubectl get pods -o name` ]]; do
 done
 
 POD_NAME=$(kubectl get pods -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
-echo Now open http://127.0.0.1:8001/api/v1/proxy/namespaces/default/pods/$POD_NAME/
-kubectl proxy
+echo Pod name $POD_NAME
+#echo Now open http://127.0.0.1:8001/api/v1/proxy/namespaces/default/pods/$POD_NAME/
+#kubectl proxy
+
+echo ...Exposing external service...
+kubectl expose rc/kubermarkets --type="NodePort" --port 80
+NODE_PORT=$(kubectl get svc/kubermarkets -o go-template='{{(index .spec.ports 0).nodePort}}')
+CLUSTER_IP=$(minikube ip)
+echo Service is going to be exposed at http://$CLUSTER_IP:$NODE_PORT
+minikube service kubermarkets --url
 
 # for cleanup
 #minikube delete
