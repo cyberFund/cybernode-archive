26e8830319103f6428309e24884d3462b65c3dd7
Anatoli Babenia <anatoli@rainforce.org>
2017-09-09 08:33:43 +0300

    Use official Rust Docker image first uploaded month ago
    Speeding up Parity builds (#93)

--- images/fullnode/build-parity.sh
+++ images/fullnode/build-parity.sh
@@ -4,30 +4,55 @@
 set -e
 # set exit code to non-0 if any piped command returns error
 set -o pipefail
+# show commands as they are executed
+set -x
 
-IMAGE=fullnode-parity
+
+IMAGE=ethereum-parity
+REPO=https://github.com/paritytech/parity
 
 # absolute path to script's directory
 DIR=$(dirname $(readlink -f "$0"))
 
 echo [build] ---- building Ethereum fullnode based on Parity ----
 
-BUILDDIR="$DIR"/parity
-BINDIR=$BUILDDIR/bin
+BUILDDIR="$DIR"/parity/_build
 # caching build dependencies to speed up builds
 CACHE=$BUILDDIR/.cache
 
 echo "\
 Build Dir:   $BUILDDIR
-Binaries:    $BINDIR
 Build Cache: $CACHE"
 
-mkdir -p $BINDIR $CACHE
-
+mkdir -p $BUILDDIR $CACHE
 cd $BUILDDIR
-CACHEVOL="-v $CACHE:/root/.cargo"
 
-docker build --no-cache $CACHEVOL -t ${IMAGE}-build -f Dockerfile-build . | tee buildimage.log
+# cloning sources
+CLONE=yes
+if [ -d parity ] ; then
+  echo "old checkout detected"
+  read -p "remove (y/n)?" choice
+  case "$choice" in
+    y|Y ) rm -rf parity;;
+    n|N ) CLONE=no;;
+    * ) exit 1;;
+  esac
+fi
+
+if [ "$CLONE" == "yes" ]; then
+  git clone https://github.com/paritytech/parity
+fi
+REVISION=`git -C parity rev-parse --short=8 HEAD`
+TAG="v`date +%Y%m%d`-$REVISION"
+echo $TAG > VERSION
+
+
+# https://github.com/rust-lang-nursery/docker-rust/blob/bf30ee63/1.20.0/stretch/Dockerfile
+CACHEVOL="-v $CACHE:/usr/local/cargo"
+
+docker run --rm --user "$(id -u)":"$(id -g)" -v "$BUILDDIR"/parity:/build -w /build rust:1.20-stretch cargo build --release
+
+#docker build --no-cache $CACHEVOL -t ${IMAGE}-build -f Dockerfile-build . | tee buildimage.log
 #docker run --rm -v $BINDIR:/build ${IMAGE}-build | tee buildimage-run.log
 #echo ... built btcd binaries:
 #ls -la $BINDIR
