b3c9f8ce22262e13b696fb90ce88011c12602793
Anatoli Babenia <anatoli@rainforce.org>
2017-09-12 18:27:43 +0300

    build-parity.sh: $CLONEDIR and pinned Rust toolchain version
    Fail: https://travis-ci.org/cyberFund/cybernode/builds/273541206#L713
    Fix: https://github.com/rust-lang-nursery/docker-rust/issues/8#issuecomment-328866883

--- images/fullnode/build-parity.sh
+++ images/fullnode/build-parity.sh
@@ -17,6 +17,7 @@ DIR=$(dirname $(readlink -f "$0"))
 echo [build] ---- building Ethereum fullnode based on Parity ----
 
 BUILDDIR="$DIR"/parity/_build
+CLONEDIR=$BUILDDIR/parity
 # caching build dependencies to speed up builds
 CACHE=$BUILDDIR/.cache
 
@@ -29,28 +30,31 @@ cd $BUILDDIR
 
 # cloning sources
 CLONE=yes
-if [ -d parity ] ; then
+if [ -d $CLONEDIR ] ; then
   echo "old checkout detected"
   read -p "remove (y/n)?" choice
   case "$choice" in
-    y|Y ) rm -rf parity;;
+    y|Y ) rm -rf $CLONEDIR;;
     n|N ) CLONE=no;;
     * ) exit 1;;
   esac
 fi
 
 if [ "$CLONE" == "yes" ]; then
-  git clone https://github.com/paritytech/parity
+  git clone https://github.com/paritytech/parity $CLONEDIR
 fi
-REVISION=`git -C parity rev-parse --short=8 HEAD`
+REVISION=`git -C $CLONEDIR rev-parse --short=8 HEAD`
 TAG="v`date +%Y%m%d`-$REVISION"
 echo $TAG > VERSION
 
+echo Patch Rust toolchain version for repeatable builds
+# https://github.com/rust-lang-nursery/docker-rust/issues/8
+echo 1.20.0 > $CLONEDIR/rust-toolchain
 
 # https://github.com/rust-lang-nursery/docker-rust/blob/bf30ee63/1.20.0/stretch/Dockerfile
 CACHEVOL="-v $CACHE:/usr/local/cargo"
 
-docker run --rm --user "$(id -u)":"$(id -g)" -v "$BUILDDIR"/parity:/build -w /build rust:1.20-stretch cargo build --release
+docker run --rm --user "$(id -u)":"$(id -g)" -v $CLONEDIR:/build -w /build rust:1.20-stretch cargo build --release
 
 #docker build --no-cache $CACHEVOL -t ${IMAGE}-build -f Dockerfile-build . | tee buildimage.log
 #docker run --rm -v $BINDIR:/build ${IMAGE}-build | tee buildimage-run.log
