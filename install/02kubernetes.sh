9dda0a0cfca1f891c4575072173660521c33b8db
Anatoli Babenia <anatoli@rainforce.org>
2017-09-13 17:46:29 +0300

    Install docker machine kvm driver for Ubuntu

--- install/02kubernetes.sh
+++ install/02kubernetes.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
-# Install minukube and kubectl globally to /usr/local/bin
+# Install minikube, kvm driver and kubectl globally to /usr/local/bin
 
-echo [x] Install kubectl and minikube to /usr/local/bin
+echo [x] Install kubectl, minikube and kvm driver to /usr/local/bin
 
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
 
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
+
+# detect OS
+. /etc/os-release
+
+if [ $ID = "ubuntu" ]; then
+    BIN=docker-machine-driver-kvm
+    echo Fetching $BIN for $NAME
+    curl -Lo $BIN https://github.com/dhiltgen/docker-machine-kvm/releases/download/v0.10.0/docker-machine-driver-kvm-ubuntu16.04 && chmod +x $BIN && sudo mv $BIN /usr/local/bin/
+fi
 
